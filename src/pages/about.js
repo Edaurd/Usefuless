668d0e460458e48702308551fbe5ea1451038d23
Eduard <sparqeey@gmail.com>
2023-01-06 17:54:03 +0100

    Finished react in Gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Us">
+      <p>
+        Usefuless was founded in 3023 by founder, 𝙹ᒲ╎ᓵ∷𝙹リ !¡ᒷ∷ᓭᒷ╎ ᒷ╎⊣⍑ℸ ̣. after
+        the Omicronians took over this planet they managed to create a profound
+        obsession over Magikarp's Useles and Gyarados's Useful abilities and
+        their Pokemon card equivalants.
+      </p>
+    </Layout>
+  )
+}
+
+export default AboutPage
